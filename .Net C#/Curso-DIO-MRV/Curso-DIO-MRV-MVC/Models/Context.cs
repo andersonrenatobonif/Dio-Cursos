bcf332505577871f57dd2ed4222e8a99b510994d
andersonrenatobonif <arbssantos@gmail.com>
2021-06-10 23:00:29 -0300

    Realiza atividade em MVC e Criação de API

---
+++ .Net C#/Curso-DIO-MRV/Curso-DIO-MRV-MVC/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Curso_DIO_MRV_MVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Data Source=ANDERSON\SQLEXPRESS;Initial Catalog=Curso-dioMRVmvc;Integrated Security=True");
+        }
+    }
+}
---
+++ .Net C#/Curso-DIO-MRV/Curso-DIO-MRV-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Curso_DIO_MRV_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
