799b00b337ded81dbc8d68458b7e672f38614a71
andersonrenatobonif <arbssantos@gmail.com>
2021-06-24 18:10:42 -0300

    Adiciona proteção à API utiizando JwtBearer

---
+++ .Net C#/Estrutura-Back-End/Estrutura-back-end-API/Controllers/CursoController.cs
@@ -0,0 +1,59 @@
+﻿using Estrutura_back_end_API.Models;
+using Estrutura_back_end_API.Models.Cursos;
+using Estrutura_back_end_API.Models.Usuarios;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Estrutura_back_end_API.Controllers
+{
+    [Route("api/v1/cursos")]
+    [ApiController]
+    [Authorize]
+    public class CursoController : ControllerBase
+    {
+        /// <summary>
+        /// Este serviço permite cadastrar curso para o usuario autenticado 
+        /// </summary>
+        /// <returns>Retorna status 201 e dados do curso do usuario</returns>"
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao cadastrar um curso", Type = typeof(LoginViewModelInput))]
+        [SwaggerResponse(statusCode: 400, description: "Não autorizado", Type = typeof(ValidaCampoViewModelOutput))]
+       
+        [HttpPost]
+        [Route(" ")]
+
+        public async Task<IActionResult> Post(CursoViewModelInput cursoViewModelInput)
+        {
+            var codigoUsuario = int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
+            return Created("", cursoViewModelInput);
+        }
+
+        /// <summary>
+        /// Este serviço permite obter todos os cursos ativos do usuario 
+        /// </summary>
+        /// <returns>Retorna status ok e dados do curso do usuario</returns>"
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao obter curso", Type = typeof(LoginViewModelInput))]
+        [SwaggerResponse(statusCode: 400, description: "Não autorizado", Type = typeof(ValidaCampoViewModelOutput))]
+
+        [HttpGet]
+        [Route(" ")]
+
+        public async Task<IActionResult> Get(CursoViewModelInput cursoViewModelInput)
+        {
+            var cursos = new List<CursoViewModelOutput>();
+            cursos.Add(new CursoViewModelOutput()
+            {
+                Login = codigoUsuario.ToString(),
+                Descricao = "teste",
+                Nome = "teste"
+            });
+            return Ok(cursos);
+        }
+    }
+}
--- .Net C#/Estrutura-Back-End/Estrutura-back-end-API/Controllers/UsuarioController.cs
+++ .Net C#/Estrutura-Back-End/Estrutura-back-end-API/Controllers/UsuarioController.cs
@@ -3,10 +3,14 @@ using Estrutura_back_end_API.Models;
 using Estrutura_back_end_API.Models.Usuarios;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Annotations;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Estrutura_back_end_API.Controllers
@@ -17,6 +21,7 @@ namespace Estrutura_back_end_API.Controllers
     
     public class UsuarioController : ControllerBase
     {
+        private object usuarioViewModelOutput;
 
         /// <summary>
         /// Este serviço permite autenticar se um usuário cadastrado é ativo 
@@ -36,7 +41,31 @@ namespace Estrutura_back_end_API.Controllers
             //{
             //    return BadRequest(new ValidaCampoViewModelOutput(ModelState.SelectMany(sm => sm.Value.Errors).Select(s => s.ErrorMessage)));
             //}
-            return Ok(loginViewModelInput);
+
+           
+            var secret = Encoding.ASCII.GetBytes("MzfsT&d9gprP>!9$Es(X!5g@;ef!5sbk:jH\\2.}8ZP'qY#7");
+            var symmetricSecurityKey = new SymmetricSecurityKey(secret);
+            var securityTokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, usuarioViewModelOutput.Codigo.ToString()),
+                    new Claim(ClaimTypes.Name, usuarioViewModelOutput.Login.ToString()),
+                    new Claim(ClaimTypes.Email, usuarioViewModelOutput.Email.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var tokenGenerated = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+            var token = jwtSecurityTokenHandler.WriteToken(tokenGenerated);
+
+
+            return Ok(new
+            {
+                Token = token,
+                Usuario = usuarioViewModelOutput
+            });
         }
 
         [HttpPost]
