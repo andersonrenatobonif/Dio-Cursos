387b6002d12885c91f9dcdcdab9992f7549e1521
andersonrenatobonif <arbssantos@gmail.com>
2021-06-26 15:15:18 -0300

    adiciona início de criação de aplicação de transferências bancárias

---
+++ .Net C#/App-Transferencia-Bancaria/DIO.Bank/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DIO.Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta minhaConta = new Conta();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
