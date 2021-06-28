98804b217e1af940c6f0477bd7d9d1073b265596
andersonrenatobonif <arbssantos@gmail.com>
2021-06-28 19:29:43 -0300

    Adiciona método inserir nova conta

--- .Net C#/App-Transferencia-Bancaria/DIO.Bank/Program.cs
+++ .Net C#/App-Transferencia-Bancaria/DIO.Bank/Program.cs
@@ -1,13 +1,99 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DIO.Bank
 {
     class Program
     {
+        static List<Conta> listContas = new List<Conta>();
+
         static void Main(string[] args)
         {
-            Conta minhaConta = new Conta();
-            Console.WriteLine("Hello World!");
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "x")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //ListarContas();
+                        break;
+
+                    case "2":
+                        InserirNovaConta();
+                        break;
+                    case "3":
+                        //Transferir();
+                        break;
+
+                    case "4":
+                        //Sacar();
+                        break;
+
+                    case "5":
+                        //Depositar();
+                        break;
+
+                    case "C":
+                        Console.Clear();
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+            Console.WriteLine("Obrigado por utilizar nossos serviços. ");
+            Console.ReadLine();
+        }
+
+        private static void InserirNovaConta()
+        {
+            Console.WriteLine("Inserindo nova conta");
+            Console.WriteLine();
+
+            Console.Write("Digite 1 para pessoa física ou 2 para pessoa jurídica: ");
+            int entradaTipoConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Nome do Cliente: ");
+            string entradaNome = Console.ReadLine();
+
+            Console.Write("Digite o saldo inicial: ");
+            double entradaSaldo = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Crédito: ");
+            double entradaCredito = double.Parse(Console.ReadLine());
+
+            Conta novaConta = new Conta(tipoConta: (TipoConta)entradaTipoConta,
+                                                    saldo: entradaSaldo, 
+                                                    credito: entradaCredito,
+                                                    nome: entradaNome);
+
+            listContas.Add(novaConta);
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("DIO Bank a sey dispor!");
+            Console.WriteLine("Informe a opção desejada: ");
+            
+            Console.WriteLine("1- Listar contas: ");
+            Console.WriteLine("2- Inserir nova conta: ");
+            Console.WriteLine("3- Transferir: ");
+            Console.WriteLine("4- Sacar: ");
+            Console.WriteLine("5- Depositar: ");
+            Console.WriteLine("C- Limpar tela: ");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+
         }
     }
 }
