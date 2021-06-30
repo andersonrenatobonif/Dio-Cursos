7422e8e5d3ede030f34e74d9a9b055f524da508c
andersonrenatobonif <arbssantos@gmail.com>
2021-06-29 22:11:36 -0300

    Implementa testes de unidade ao projeto

---
+++ .Net C#/Estrutura-Back-End/Tests/curso.api.tests/Integrations/Controllers/UsuarioControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using Estrutura_back_end_API;
+using Estrutura_back_end_API.Models.Usuarios;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace curso.api.tests.Integrations
+{
+    public class UsuarioControllerTests
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        private readonly HttpClient _httpClient;
+        public UsuarioControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+            HttpClient httpClient = _factory.CreateClient();
+        }
+
+        //WhenGivenThen
+        //Quando_Dado_EntaoResultadoEsperado
+        [Fact]
+        public void Logar()
+        {
+            //Arrange
+            var loginViewModelInput = new LoginViewModelInput
+            {
+                Login = "andersonsantos",
+                Senha = "123445"
+            };
+            StringContent content = new StringContent(JsonConvert.SerializeObject(loginViewModelInput));
+
+            //Act
+            var httpClientRequest = _httpClient.PostAsync("api/v1/usuario/logar", content).GetAwaiter().GetResult();
+
+            // Assert
+            Assert.Equal(System.Net.HttpStatusCode.OK, httpClientRequest.StatusCode);
+        }
+    }
+}
