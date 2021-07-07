7d3cdf062fc73f31c8b109586df006adf94eb5b4
andersonrenatobonif <arbssantos@gmail.com>
2021-07-07 17:14:10 -0300

    adiciona Api de inserção de dados de infectados pelo coronavírus

---
+++ .Net C#/ApiMongoDB/Controllers/InfectadoController.cs
@@ -0,0 +1,40 @@
+using ApiMongoDB.Data.Collections;
+using ApiMongoDB.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+//using ApiMongoDB.Data;
+
+namespace ApiMongoDB.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectadosCollection = mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+            _infectadosCollection.InsertOne(infectado);
+            
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+            
+            return Ok(infectados);
+        }
+    }
+}
